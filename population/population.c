22c6bedfca09a8244545c079258ccffcaecb9aef
ItsCrousie <itscrousie@gmail.com>
2023-04-25 16:59:25 -0400

    Submit: Lab-1

--- population/population.c
+++ population/population.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
 #include <cs50.h>
 
-long calcPopulationGrowth(long starting, long ending);
-
 int main(void)
 {
     // TODO: Prompt for start size
-    long startingPopulation;
+    long start;
     do
     {
-        startingPopulation = get_long("Enter Starting Population: ");
+        start = get_long("Enter Starting Population: ");
     }
-    while (startingPopulation < 9);
+    while (start < 9);
 
     // TODO: Prompt for end size
-    long endingPopulation;
+    long end;
     do
     {
-        endingPopulation = get_long("Enter Ending Population: ");
-    } while (endingPopulation < startingPopulation);
+        end = get_long("Enter Ending Population: ");
+    } while (end < start);
 
     // TODO: Calculate number of years until we reach threshold
-    long years = calcPopulationGrowth(startingPopulation, endingPopulation);
-    // TODO: Print number of years
-    printf("Start size: %li\nEnd size:%li\nYears: %li\n", startingPopulation, endingPopulation, years);
-}
+    int years = 0;
+    //update by reference start becuase final needs to show initial value.
+    int i = start;
+    do
+    {
+        i += (i/3) - (i/4);
+        years++;
+    } while (i < end);
 
-long calcPopulationGrowth(long starting, long ending)
-{
-    return (16 * starting) / (3 * ending);
+    // TODO: Print number of years
+    printf("Start size: %li\nEnd size:%li\nYears: %i\n", start, end, years);
 }
